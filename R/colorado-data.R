b6f8e11a7dcafd274408e1458478cd227d4a1d5e
Joshua French <joshua.french@gmail.com>
2023-08-21 09:53:32 -0600

    Fix url in colorado data

--- R/colorado-data.R
+++ R/colorado-data.R
@@ -1,6 +1,12 @@
 #' @name colorado
 #' @title Colorado precipitation data
-#' @description Data related to Colorado precipitation in May 1997. Taken from \url{http://www.image.ucar.edu/Data/US.monthly.met/}. Data is contained in a list with components \code{odata} (containing a transformed precipitation variable) and \code{ocoords} containing the longitude and latitude of the associated sites.
+#' @description Data related to Colorado precipitation in
+#'   May 1997. Taken from
+#'   \url{https://www.image.ucar.edu/Data/US.monthly.met/}.
+#'   Data is contained in a list with components
+#'   \code{odata} (containing a transformed precipitation
+#'   variable) and \code{ocoords} containing the longitude
+#'   and latitude of the associated sites.
 #' @docType data
 #' @usage data(colorado)
 #' @format A list.
